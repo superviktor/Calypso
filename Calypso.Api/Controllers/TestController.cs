89d3383ef7da4e12f86b2820790918771490e2d1
Viktor Prykhidko <viktor.prykhidko@softwarium.net>
2021-06-22 20:13:33 +0300

    fix docker compose; added test controller

---
+++ Calypso.Api/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Calypso.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("I am ok");
+        }
+    }
+}
