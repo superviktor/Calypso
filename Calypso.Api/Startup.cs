562773917489db39a413125b40b1656b711def6f
Viktor Prykhidko <viktor.prykhidko@softwarium.net>
2021-06-24 17:08:47 +0300

    Added swagger for all envs

--- Calypso.Api/Startup.cs
+++ Calypso.Api/Startup.cs
@@ -37,12 +37,10 @@ namespace Calypso.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Calypso.Api v1"));
-            }
+
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Calypso.Api v1"));
 
             app.UseRouting();
 
