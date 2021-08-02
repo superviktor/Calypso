3e49bc886c9fb9c62b6e5e5309569ffb040406fc
Viktor Prykhidko <viktor.prykhidko@softwarium.net>
2021-08-02 16:53:01 +0300

    list and get

---
+++ Calypso.TeamsApp/Models/Feedback.cs
@@ -0,0 +1,21 @@
+﻿namespace Calypso.TeamsApp.Models
+{
+    public class Feedback
+    {
+        public string RowKey { get; set; }
+        public int Number { get; set; }
+        public string Description { get; set; }
+        public string Subject { get; set; }
+        public string Machine { get; set; }
+        public string Location { get; set; }
+        public string Reporter { get; set; }
+        public string Role { get; set; }
+        public string Date { get; set; }
+        public string Sbu { get; set; }
+        public string ProjectName { get; set; }
+        public string ProductName { get; set; }
+        public string Factory { get; set; }
+        public string FileName { get; set; }
+        public string Status { get; set; }
+    }
+}
